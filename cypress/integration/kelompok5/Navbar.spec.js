26f6c99386da453af41f1bbef5db80cb4ca2fdd0
divardhia <divardhiass@gmail.com>
2021-11-24 15:00:40 +0700

    membenarkan Navbar,spec.js

--- cypress/integration/kelompok5/Navbar.spec.js
+++ cypress/integration/kelompok5/Navbar.spec.js
@@ -2,59 +2,45 @@
 
 describe('Navbar', ()=>{
     beforeEach(() => { 
-        // Login Terlebih dahulu
- cy.visit('https://www.saucedemo.com/');
+    // Login Terlebih dahulu
+    cy.visit('https://www.saucedemo.com/');
+      cy.get('#user-name').type('standard_user');
+      cy.get('#password').type('secret_sauce');
+      cy.get('#login-button').click();
     });
-    cy.visit('https://www.saucedemo.com/');
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
     
-     //Assertion
-     cy.url().should('include', '/inventory.html');
-});
-
-it('Navbar', () => {
-    cy.get('button[name = ]').click();
-});
-
-
-it('All Item', () => {
-    cy.get('button[name = ]').click();
-});
-
-
-it('About', () => {
-    cy.get('button[name = ]').click();
-
-  //Assertion
-    cy.visit('https://www.saucelabs.com/');
+    it('All Item', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#inventory_sidebar_link').click();
+    });
     
+    
+    it('About', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#about_sidebar_link').click();
+    
+      //Assertion
+       
+        
+    });
+    
+    it('Logout', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#logout_sidebar_link').click();
+    
+        //Assertion
+        
+    
+    });
+    
+    
+    it('Reset APP state', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#reset_sidebar_link').click();
+    
+      //Assertion
+        cy.get('button[name = "add-to-cart-sauce-labs-backpack"]').should('be.visible');
+    });
+     
 });
 
-it('Logout', () => {
-    cy.get('#logout-button').click();
-
-    //Assertion
-    cy.visit('https://www.saucedemo.com/');
-
-});
-
-
-it('Reset APP state', () => {
-    cy.get('button[name = "add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('button[name = "remove-sauce-labs-backpack"]').click();
-
-  //Assertion
-    cy.get('button[name = "add-to-cart-sauce-labs-backpack"]').should('be.visible');
-});
- 
-it('valid username password', () => {
-    cy.get('#user-name').type('Problem-user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
-
-    //Assertion
-    cy.get('button[name = About ]').click();
-    cy.visit('https://saucelabs.com/error/404');
-});
